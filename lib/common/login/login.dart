dd95993283f307605bddb70460c021ad96817beb
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-12 18:59:18 +0100

    login page init

---
+++ lib/common/login/login.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                'Login',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
