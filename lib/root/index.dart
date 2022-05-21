6664bccedc9aa2a2c6f921ff2c0b39855028d51e
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:11:46 +0100

    root screen

---
+++ lib/root/index.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:online_market/auth/complete_profile/complete_profile.dart';
+import 'package:online_market/auth/login/login.dart';
+import 'package:online_market/auth/providers/auth_provider.dart';
+import 'package:online_market/auth/signup/signup.dart';
+import 'package:online_market/home/base.dart';
+import 'package:provider/provider.dart';
+
+class AuthRoot extends StatelessWidget {
+  const AuthRoot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Authentication>(builder: (_, auth, __) {
+      // ignore: avoid_print
+      print('Auth state is');
+      // ignore: avoid_print
+      print(auth.loginState);
+      switch (auth.loginState) {
+        case AuthState.loggedIn:
+          return BaseScreen();
+        case AuthState.loggedOut:
+          return Login();
+        case AuthState.incomplete:
+          return CompleteProfile();
+        case AuthState.signup:
+          return SignUp();
+        case AuthState.login:
+          return Login();
+        case AuthState.loading:
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        default:
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+      }
+      //return user != null ? HomeScreen() : Register();
+    });
+  }
+}
