c7349d6d4fa2029f5c6789a2049faa92f9ada953
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:30:23 +0100

    firebase options added

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,25 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:online_market/auth/login/login.dart';
 import 'package:online_market/auth/providers/auth_provider.dart';
+import 'package:online_market/firebase_options.dart';
 import 'package:online_market/util/theme.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- 
 
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   Provider.debugCheckInvalidValueType = null;
-  runApp( MultiProvider(
-    providers: [ChangeNotifierProvider(create: (context) => Authentication())],
-    child: const MyApp(),),);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Authentication())
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
