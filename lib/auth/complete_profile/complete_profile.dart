b4c63f3feb117af8098eb376a7bfe3c705db7513
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 20:50:53 +0100

    functionalities added. Almost done

--- lib/auth/complete_profile/complete_profile.dart
+++ lib/auth/complete_profile/complete_profile.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/country_picker_dialog.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:online_market/auth/models/location.dart';
+import 'package:online_market/auth/providers/auth_provider.dart';
 import 'package:online_market/util/contstants.dart';
 import 'package:online_market/util/helper.dart';
 import 'package:online_market/util/widgets/custom_buttons.dart';
+import 'package:provider/provider.dart';
+
+import '../models/user_model.dart';
 
 class CompleteProfile extends StatefulWidget {
   CompleteProfile({Key? key}) : super(key: key);
@@ -11,9 +18,31 @@ class CompleteProfile extends StatefulWidget {
 }
 
 class _CompleteProfileState extends State<CompleteProfile> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+
+  final TextEditingController _regionController = TextEditingController();
+  final TextEditingController _townController = TextEditingController();
+  final TextEditingController _quarterController = TextEditingController();
+
+  String initialCountry = 'CM';
+  String? locale;
+  String number = '';
+
+  String accounttype = '';
   final _formKey = GlobalKey<FormState>();
+  late Authentication auth;
+  late UserModel userModel;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth = Provider.of<Authentication>(context, listen: false);
+    userModel = auth.loggedUser!;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,49 +57,122 @@ class _CompleteProfileState extends State<CompleteProfile> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Login',
+                    'Complete Profile',
                     style: Theme.of(context).textTheme.headline1,
                   ),
-                  const SizedBox(height: Constants.bigAuthSpace),
+                  const SizedBox(height: Constants.bigAuthSpace / 2),
                   Constants.formBox(
+                    context: context,
                     child: TextFormField(
-                        controller: _emailController,
-                        decoration: Constants.inputDecoration('email')),
+                        style: Theme.of(context).textTheme.bodyText2,
+                        controller: _firstNameController,
+                        decoration: Constants.inputDecoration('first name')),
                   ),
                   const SizedBox(height: 10),
                   Constants.formBox(
+                    context: context,
                     child: TextFormField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: Constants.inputDecoration('password')),
+                        style: Theme.of(context).textTheme.bodyText2,
+                        controller: _lastNameController,
+                        decoration: Constants.inputDecoration('last name')),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Row(
-                      children: [
-                        Text('Forgot password? '),
-                        Icon(Icons.arrow_forward)
-                      ],
+                  const SizedBox(height: 10),
+                  Constants.formBox(
+                    context: context,
+                    child: IntlPhoneField(
+                      style: Theme.of(context).textTheme.bodyText2,
+                      controller: _phoneNumberController,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      dropdownIconPosition: IconPosition.leading,
+                      initialCountryCode: initialCountry,
+                      pickerDialogStyle: PickerDialogStyle(),
+                      decoration: const InputDecoration(
+                        // hintText: 'Phone Number',
+                        labelText: 'Phone Number',
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      onChanged: (phone) {
+                        print(phone.completeNumber);
+                        setState(() {
+                          number = phone.completeNumber;
+                        });
+                      },
+                      onCountryChanged: (country) {
+                        print('Country changed to: ' + country.name);
+                      },
                     ),
                   ),
+                  const SizedBox(height: 10),
+                  Constants.formBox(
+                    context: context,
+                    child: TextFormField(
+                        style: Theme.of(context).textTheme.bodyText2,
+                        controller: _regionController,
+                        decoration: Constants.inputDecoration('region')),
+                  ),
+                  const SizedBox(height: 10),
+                  Constants.formBox(
+                    context: context,
+                    child: TextFormField(
+                        style: Theme.of(context).textTheme.bodyText2,
+                        controller: _townController,
+                        decoration: Constants.inputDecoration('town')),
+                  ),
+                  const SizedBox(height: 10),
+                  Constants.formBox(
+                    context: context,
+                    child: TextFormField(
+                        style: Theme.of(context).textTheme.bodyText2,
+                        controller: _quarterController,
+                        decoration: Constants.inputDecoration('quarter')),
+                  ),
+                  const SizedBox(height: 20),
                   CustomButton(
-                      child: Text('Finish'),
+                      child: const Text('Finish'),
                       onTap: () {
-                        var emailGood = validateEmail(_emailController.text);
-                        var passwordGood =
-                            validatePassword(_passwordController.text);
-                        // auth.setAuthState(AuthState.loggedIn);
-                        if (emailGood != null) {
-                          showSnackBar(context, emailGood);
-                        } else if (passwordGood != null) {
-                          showSnackBar(context, passwordGood);
-                        } else {
-                          showSnackBar(context, 'Done');
-                          showProgress(context, 'Signing in', true);
-                          // auth
-                          //     .login(
-                          //         _emailController.text, _passwordController.text)
-                          //     .then((value) => hideProgress());
+                        if (_formKey.currentState!.validate()) {
+                          var firstNameGood = validateGivenNames(
+                              _firstNameController.text, true);
+                          var lastNameGood = validateGivenNames(
+                              _firstNameController.text, false);
+                          var regionGood = validateLocation(
+                              _regionController.text, 'region');
+
+                          var townGood =
+                              validateLocation(_townController.text, 'town');
+
+                          var quarterGood = validateLocation(
+                              _quarterController.text, 'quarter');
+
+                          if (firstNameGood != null) {
+                            showSnackBar(context, firstNameGood);
+                          } else if (lastNameGood != null) {
+                            showSnackBar(context, lastNameGood);
+                          } else if (regionGood != null) {
+                            showSnackBar(context, regionGood);
+                          } else if (townGood != null) {
+                            showSnackBar(context, townGood);
+                          } else if (quarterGood != null) {
+                            showSnackBar(context, quarterGood);
+                          } else {
+                            UserLocation location = UserLocation(
+                                id: userModel.uid,
+                                region: _regionController.text,
+                                town: _townController.text,
+                                quarter: _quarterController.text);
+                            userModel.firstName = _firstNameController.text;
+                            userModel.lastName = _lastNameController.text;
+                            userModel.phoneNumber = _phoneNumberController.text;
+                            userModel.completedProfile = true;
+                            userModel.location = location.toJson();
+                            showProgress(context, 'Saving data.. ', true);
+                            auth
+                                .completeProfile(userModel)
+                                .then((value) => hideProgress());
+                            showSnackBar(context, 'OK');
+                          }
                         }
                       })
                 ],
