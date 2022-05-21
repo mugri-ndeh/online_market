ee59cdac1e52f9cc7759c32a2f8b48d8d3c0ee3d
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:09:22 +0100

    user model init

---
+++ lib/auth/models/user_model.dart
@@ -0,0 +1,55 @@
+class UserModel {
+  String uid;
+  String firstName;
+  String lastName;
+  String username;
+  String email;
+  String password;
+  Map<String, dynamic>? location;
+  bool completedProfile;
+  String phoneNumber;
+  String accountType;
+
+  UserModel({
+    this.completedProfile = false,
+    this.uid = '',
+    this.firstName = '',
+    this.lastName = '',
+    this.username = '',
+    this.password = '',
+    this.location,
+    this.email = '',
+    this.phoneNumber = '',
+    this.accountType = '',
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      uid: json['uid'],
+      email: json['email'],
+      username: json['username'],
+      password: json['password'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      location: json['location'],
+      completedProfile: json['completedProfile'],
+      phoneNumber: json['phoneNumber'],
+      accountType: json['accountType'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'username': username,
+      'phoneNumber': phoneNumber,
+      'uid': uid,
+      'firstName': firstName,
+      'lastName': lastName,
+      'location': location,
+      'password': password,
+      'completedProfile': completedProfile,
+      'accountType': accountType,
+    };
+  }
+}
