b254c8113c64f550e8925209f5ca67fd6ac2c3bb
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:12:00 +0100

    shop screen init

---
+++ lib/shop/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopIndex extends StatefulWidget {
+  ShopIndex({Key? key}) : super(key: key);
+
+  @override
+  State<ShopIndex> createState() => _ShopIndexState();
+}
+
+class _ShopIndexState extends State<ShopIndex> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
