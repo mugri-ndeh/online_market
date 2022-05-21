b157a8034b5dcdd3ad9475b9e5f084aeefcaff31
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 20:49:16 +0100

    login almost done

--- lib/auth/login/login.dart
+++ lib/auth/login/login.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:online_market/auth/providers/auth_provider.dart';
 import 'package:online_market/util/contstants.dart';
 import 'package:online_market/util/helper.dart';
 import 'package:online_market/util/palette.dart';
 import 'package:online_market/util/widgets/custom_buttons.dart';
+import 'package:provider/provider.dart';
 
 class Login extends StatefulWidget {
   Login({Key? key}) : super(key: key);
@@ -15,7 +17,15 @@ class _LoginState extends State<Login> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-  // late Authentication auth;
+  late Authentication auth;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth = Provider.of<Authentication>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,28 +45,38 @@ class _LoginState extends State<Login> {
                   ),
                   const SizedBox(height: Constants.bigAuthSpace),
                   Constants.formBox(
+                    context: context,
                     child: TextFormField(
+                        style: Theme.of(context).textTheme.bodyText2,
                         controller: _emailController,
                         decoration: Constants.inputDecoration('email')),
                   ),
                   const SizedBox(height: 10),
                   Constants.formBox(
+                    context: context,
                     child: TextFormField(
+                        style: Theme.of(context).textTheme.bodyText2,
                         controller: _passwordController,
                         obscureText: true,
                         decoration: Constants.inputDecoration('password')),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Row(
-                      children: [
-                        Text('Forgot password? '),
-                        Icon(Icons.arrow_forward)
-                      ],
-                    ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text('Forgot password?'),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_forward,
+                          color: Palette.buttonColor,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
                   ),
+                  const SizedBox(height: 10),
                   CustomButton(
-                      child: Text('Login'),
+                      child: const Text('Login'),
                       onTap: () {
                         var emailGood = validateEmail(_emailController.text);
                         var passwordGood =
@@ -69,12 +89,30 @@ class _LoginState extends State<Login> {
                         } else {
                           showSnackBar(context, 'Done');
                           showProgress(context, 'Signing in', true);
-                          // auth
-                          //     .login(
-                          //         _emailController.text, _passwordController.text)
-                          //     .then((value) => hideProgress());
+                          auth
+                              .login(_emailController.text,
+                                  _passwordController.text)
+                              .then((value) => hideProgress());
                         }
-                      })
+                      }),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Don't have account? "),
+                        GestureDetector(
+                          onTap: () {
+                            auth.setAuthState(AuthState.signup);
+                          },
+                          child: Text(
+                            'Sign up',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
