cecfcb71b66bb5f95b0778ae6a7a20710e2d7d0d
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:09:50 +0100

    favourites index page init

---
+++ lib/favourites/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavouritesIndex extends StatefulWidget {
+  FavouritesIndex({Key? key}) : super(key: key);
+
+  @override
+  State<FavouritesIndex> createState() => _FavouritesIndexState();
+}
+
+class _FavouritesIndexState extends State<FavouritesIndex> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
