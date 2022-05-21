968334abfc88eee4265298f3c4b8dfb7f083ef19
mugri-ndeh <frunwindeh@yahoo.com>
2022-05-21 15:10:38 +0100

    wish list index init

---
+++ lib/wish_list/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WishListIndex extends StatefulWidget {
+  WishListIndex({Key? key}) : super(key: key);
+
+  @override
+  State<WishListIndex> createState() => _WishListIndexState();
+}
+
+class _WishListIndexState extends State<WishListIndex> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
