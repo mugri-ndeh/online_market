9924d604999168fa625d6a58fd5fde266ab08020
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 10:56:46 +0100

    int prod

---
+++ lib/model/product.dart
@@ -0,0 +1,16 @@
+class Product {
+  int id;
+  String name;
+  String category;
+  String price;
+  String image;
+  String qty;
+  Product({
+    this.id = 0,
+    this.name = '',
+    this.category = '',
+    this.price = '',
+    this.image = '',
+    this.qty = '',
+  });
+}
