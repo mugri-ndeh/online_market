db05ffff254f2d75420f84625820ce1cfaea6e89
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-08 10:56:57 +0100

    init shopping order

--- lib/model/shopping_order.dart
+++ lib/model/shopping_order.dart
@@ -1,6 +1,18 @@
 class ShoppingOrder {
+  int id;
+  int customerId;
+  int shopId;
+  int productId;
+  String date;
   String state;
+  List items;
   ShoppingOrder({
+    this.id = 0,
     this.state = '',
+    this.customerId = 0,
+    this.shopId = 0,
+    this.productId = 0,
+    this.date = '',
+    this.items = const [],
   });
 }
