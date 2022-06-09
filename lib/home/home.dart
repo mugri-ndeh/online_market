fde44d64b9e930f6d81d83aa7e587f7be768b43e
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-09 17:12:26 +0100

    ok

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -92,6 +92,7 @@ class _HomeState extends State<Home> {
           ),
           onPressed: () {
             Scaffold.of(context).openDrawer();
+            print("TAOPPED");
           },
         ),
         elevation: 0,
