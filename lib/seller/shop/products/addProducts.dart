bbe189deea9e15dbc962e36eab355b2427f4b88d
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-09 16:33:44 +0100

    add products page

---
+++ lib/seller/shop/products/addProducts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppProductsPage extends StatefulWidget {
+  AppProductsPage({Key? key}) : super(key: key);
+
+  @override
+  State<AppProductsPage> createState() => _AppProductsPageState();
+}
+
+class _AppProductsPageState extends State<AppProductsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
