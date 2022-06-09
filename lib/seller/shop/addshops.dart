d865a5e24a80761ddcd203e715b84a17f1272b25
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-09 16:33:06 +0100

    add shop pahe for seller init

---
+++ lib/seller/shop/addshops.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddShoppage extends StatefulWidget {
+  AddShoppage({Key? key}) : super(key: key);
+
+  @override
+  State<AddShoppage> createState() => _AddShoppageState();
+}
+
+class _AddShoppageState extends State<AddShoppage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
