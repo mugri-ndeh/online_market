f6a7a7fc46f87c9c344c46855d918ac6a53b34c5
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:40:58 +0100

    ratings init

---
+++ lib/seller/ratings/seller_ratings.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SellerRating extends StatefulWidget {
+  SellerRating({Key? key}) : super(key: key);
+
+  @override
+  State<SellerRating> createState() => _SellerRatingState();
+}
+
+class _SellerRatingState extends State<SellerRating> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: Theme.of(context).iconTheme,
+        titleTextStyle: Theme.of(context).textTheme.headline6,
+        title: Text(
+          'My Reviews',
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
