f8a68a3d73348415bb82f14a2a05b27871decfb0
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:37:58 +0100

    fixes on model

--- lib/model/shop.dart
+++ lib/model/shop.dart
@@ -2,18 +2,25 @@ class Shop {
   int id;
   String name;
   String sellerName;
+  String? shopImg;
 
-  Shop({
-    this.id = 0,
-    this.name = '',
-    this.sellerName = '',
-  });
+  Shop({this.id = 0, this.name = '', this.sellerName = '', this.shopImg});
 
   factory Shop.fromJson(Map<String, dynamic> json) {
     return Shop(
       id: json['id'],
       name: json['name'],
-      sellerName: json['username'],
+      sellerName: json['seller_id'].toString(),
+      shopImg: json['shop_img'],
+    );
+  }
+
+  factory Shop.fromSellerJson(Map<String, dynamic> json) {
+    return Shop(
+      id: json['id'],
+      name: json['name'],
+      sellerName: json['seller_id'].toString(),
+      shopImg: json['shop_img'],
     );
   }
 }
