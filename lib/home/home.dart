88e5cd4e662393725cd842b4d37988731ae8aad1
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:36:22 +0100

    better images

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -25,13 +25,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool _isFavorited = false;
   int _current = 0;
   final CarouselController _controller = CarouselController();
   final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://m.media-amazon.com/images/I/71vvXGmdKWL._AC_SL1500_.jpg',
+    'https://m.media-amazon.com/images/I/71Ps25MnkNL._AC_UY575_.jpg',
+    'https://m.media-amazon.com/images/I/817aOXLoNpL._AC_SL1500_.jpg',
+    'https://m.media-amazon.com/images/I/71rXSVqET9L._AC_SL1257_.jpg'
   ];
 
   List<Product> promo = [];
