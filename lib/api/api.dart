4293d01323daec638e9e57231b28e6488418c337
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-11 23:35:03 +0100

    added new endpoints

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -8,6 +8,7 @@ class Api {
   static const String resetPassword = baseApi + "customer/reset_password";
   static const String editProfile = baseApi + "common/edit_profile.php";
   static const String updatePassword = baseApi + "customer/update_password.php";
+  static const String getCategories = baseApi + "common/get_categories.php";
 }
 
 class CustomerApi {
@@ -21,8 +22,11 @@ class CustomerApi {
   static const String makeReview = baseApi + "customer/make_review.php";
   static const String search = baseApi + "customer/search.php";
   static const String viewStores = baseApi + "customer/view_stores.php";
+  static const String getWishList = baseApi + "customer/get_wish_list.php";
   static const String getShopProducts =
       baseApi + "customer/get_shop_products.php";
+  static const String addToWish = baseApi + "customer/add_to_wish.php";
+  static const String getOrders = baseApi + "customer/get_orders.php";
 }
 
 class SellerApi {
@@ -37,4 +41,5 @@ class SellerApi {
       baseApi + "seller/view_all_products.php";
   static const String viewProducts = baseApi + "seller/view_products.php";
   static const String viewStores = baseApi + "seller/view_stores.php";
+  static const String uploadImage = baseApi + "seller/upload_image.php";
 }
