bee9852684c243f642d37ea7c9a5b86ad4eb6b3b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-13 13:07:07 +0100

    promo int

---
+++ lib/seller/promotion/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Promotions extends StatefulWidget {
+  Promotions({Key? key}) : super(key: key);
+
+  @override
+  State<Promotions> createState() => _PromotionsState();
+}
+
+class _PromotionsState extends State<Promotions> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
